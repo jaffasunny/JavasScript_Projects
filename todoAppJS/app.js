f309b4cbb942886502dbd1bdacdc86ed3f278acc
jaffasunny <jaffer.sunny125@gmail.com>
2021-09-09 09:29:30 +0500

    todo-app-delete-func commit

--- todoAppJS/app.js
+++ todoAppJS/app.js
@@ -5,10 +5,21 @@ function addTodo() {
 	if (todo_item.value !== "") {
 		var li = document.createElement("li");
 		var li_text = document.createTextNode(todo_item.value);
-
 		li.appendChild(li_text);
+
+		var delBtn = document.createElement('button');
+		var delText = document.createTextNode('DELETE');
+		delBtn.setAttribute('class','btn');
+		delBtn.setAttribute('onclick','deleteItem(this)');
+		delBtn.appendChild(delText);
+		li.appendChild(delBtn);
+
 		ul.appendChild(li);
 
 		todo_item.value = "";
 	}
+}
+
+function deleteItem(btn){
+	btn.parentNode.remove();
 }
