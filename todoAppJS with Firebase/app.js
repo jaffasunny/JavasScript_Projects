62c018d19e64dd5f5ea271e51676ee483bb616b6
jaffasunny <jaffer.sunny125@gmail.com>
2021-10-26 13:28:39 +0500

    new es6 syntax

--- todoAppJS with Firebase/app.js
+++ todoAppJS with Firebase/app.js
@@ -1,42 +1,58 @@
 var ul = document.getElementById("added-todos");
 
 function addTodo() {
-	var todo_item = document.getElementById("todo-item");
-	if (todo_item.value !== "") {
-		var li = document.createElement("li");
-		var li_text = document.createTextNode(todo_item.value);
-		li.appendChild(li_text);
+	// Old es5 syntax
+	// var todo_item = document.getElementById("todo-item");
+	// if (todo_item.value !== "") {
+	// 	var li = document.createElement("li");
+	// 	var li_text = document.createTextNode(todo_item.value);
+	// 	li.appendChild(li_text);
+	// 	var delBtn = document.createElement('button');
+	// 	var delText = document.createTextNode('DELETE');
+	// 	delBtn.setAttribute('class','btn');
+	// 	delBtn.setAttribute('onclick','deleteItem(this)');
+	// 	delBtn.appendChild(delText);
+	// 	li.appendChild(delBtn);
+	// 	var editBtn = document.createElement('button');
+	// 	var editText = document.createTextNode('EDIT');
+	// 	editBtn.setAttribute('class','btn');
+	// 	editBtn.className += ' ml-1';
+	// 	editBtn.setAttribute('onclick','editItem(this)');
+	// 	editBtn.appendChild(editText);
+	// 	li.appendChild(editBtn);
+	// 	ul.appendChild(li);
+	// 	todo_item.value = "";
+	// }
 
-		var delBtn = document.createElement('button');
-		var delText = document.createTextNode('DELETE');
-		delBtn.setAttribute('class','btn');
-		delBtn.setAttribute('onclick','deleteItem(this)');
-		delBtn.appendChild(delText);
-		li.appendChild(delBtn);
+	// New es6 syntax
+	let todo_input = document.getElementById("todo-item");
+	let ul = document.getElementById("added-todos");
 
-		var editBtn = document.createElement('button');
-		var editText = document.createTextNode('EDIT');
-		editBtn.setAttribute('class','btn');
-		editBtn.className += ' ml-1';
-		editBtn.setAttribute('onclick','editItem(this)');
-		editBtn.appendChild(editText);
-		li.appendChild(editBtn);
+	// console.log(todos);
+	if (todo_input.value !== "") {
+		let todoInput_value = todo_input.value;
+		console.log(todoInput_value);
+		let todo = `
+		<li>
+			${todoInput_value}
+			<button class='btn' onclick=deleteItem(this)>Delete</button>
+			<button class='btn' onclick=deleteItem(this)>Edit</button>
 
-		ul.appendChild(li);
-
-		todo_item.value = "";
+		</li>
+		`;
+		ul.innerHTML += todo;
 	}
 }
 
-function deleteItem(btn){
+function deleteItem(btn) {
 	btn.parentNode.remove();
 }
 
-function deleteAll(){
-	ul.innerHTML = '';
+function deleteAll() {
+	ul.innerHTML = "";
 }
 
-function editItem(btn){
+function editItem(btn) {
 	var prompt_val = btn.parentNode.firstChild.nodeValue;
 	var editValue = prompt("Enter the edit value", prompt_val);
 	btn.parentNode.firstChild.nodeValue = editValue;
