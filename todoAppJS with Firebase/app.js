f9bf9cd4bd0a4e975c74cab7860e1e792b2395f8
jaffasunny <jaffer.sunny125@gmail.com>
2021-11-01 13:05:19 +0500

    Adding js functionality of checked

--- todoAppJS with Firebase/app.js
+++ todoAppJS with Firebase/app.js
@@ -26,6 +26,69 @@ function addItem(event) {
 	// }
 }
 
+function getItems() {
+	db.collection("todo-items").onSnapshot((snapshot) => {
+		let items = [];
+		snapshot.docs.forEach((doc) => {
+			items.push({
+				id: doc.id,
+				...doc.data(),
+			});
+		});
+		generateItems(items);
+	});
+}
+
+function generateItems(items) {
+	let itemsHtml = "";
+	items.forEach((item) => {
+		itemsHtml += `
+			<div class="todo-item">
+                <div class="check">
+                    <div data-id="${item.id}" class="check-mark" ${item.status == "completed" ? "checked": ""}>
+                        <img src="./assets/icon-check.svg" alt="">
+                    </div>
+                </div>
+                <div class="todo-text ${item.status == "completed" ? "checked": ""}">
+                    ${item.text}
+                </div>
+            </div>
+		`;
+	});
+	document.querySelector(".todo-items").innerHTML = itemsHtml;
+	createEventListeners();
+}
+
+getItems();
+
+function createEventListeners() {
+	let todoCheckMarks = document.querySelectorAll(".todo-item .check-mark");
+	todoCheckMarks.forEach((checkMark) => {
+		checkMark.addEventListener("click", function () {
+			markCompleted(checkMark.dataset.id);
+		});
+	});
+}
+
+function markCompleted(id) {
+	let item = db.collection("todo-items").doc(id);
+	item.get().then((doc) => {
+		if (doc.exists) {
+			let status = doc.data().status;
+			if (status == "active") {
+				item.update({
+					status: "completed",
+				});
+			} else if (status == "completed") {
+				item.update({
+					status: "active",
+				});
+			}
+			console.log("Here is the doc", doc.data());
+		}
+	});
+}
+
 function deleteItem(btn) {
 	btn.parentNode.remove();
 }
